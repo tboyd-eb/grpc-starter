2be5dc8632e793b52fdb56522594ba383202b1fa
Todd Boyd <tboyd@eventbrite.com>
2022-06-02 11:43:43 -0500

    sensical unit tests

--- src/test/kotlin/com/myorg/myservice/MyServiceTest.kt
+++ src/test/kotlin/com/myorg/myservice/MyServiceTest.kt
@@ -4,6 +4,7 @@ import com.myorg.myservice.controllers.CreateEventsController
 import com.myorg.myservice.proto.GetComplimentsRequest
 import com.myorg.myservice.proto.GetComplimentsResponse
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldNotBe
 import io.mockk.coEvery
 import io.mockk.mockk
 
@@ -12,18 +13,29 @@ class MyServiceTest : DescribeSpec({
     lateinit var mockCreateEventsController: CreateEventsController
 
     beforeEach {
-        mockCreateEventsController = mockk()
+        mockCreateEventsController = mockk(relaxed = true)
         myService = MyService(mockCreateEventsController)
     }
 
     describe("GetCompliments endpoint") {
-        it("should respond with a list of compliments") {
-            val expectedResponse = GetComplimentsResponse.newBuilder()
+        it("should respond") {
+            val response = myService.getCompliments(
+                GetComplimentsRequest.getDefaultInstance()
+            )
+
+            response shouldNotBe null
+        }
+    }
+
+    describe("CreateEventsController") {
+        it("should produce a list of compliments") {
+            val expectedResponse = GetComplimentsResponse
+                .newBuilder()
                 .addAllCompliments(
                     listOf("Good job!", "You rock!", "Nicely done!")
                 )
                 .build()
-            val request = GetComplimentsRequest.newBuilder().build()
+            val request = GetComplimentsRequest.getDefaultInstance()
 
             coEvery {
                 myService.getCompliments(request)
